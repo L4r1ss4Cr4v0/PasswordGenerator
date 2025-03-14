da495afe87b646bc651f8f8445f818582c2c7ff2
Larissa Cravo Carvalho Câmara Santos <larissacravoccamara@gmail.com>
2025-03-14 18:49:02 -0300

    chore: JS copy function

--- index.php
+++ index.php
@@ -9,7 +9,16 @@ if (isset($_POST["size"])) {
         for ($i = 0; $i < $size; $i++) {
             $password .= $caracters[rand(0, strlen($caracters) - 1)];
         }
-        return "<p id='pwd'>$password</p>" ;   
+        return 
+        "<div id=\"outside-div\" class=\"flex\" style=\"flex-direction: column\">
+            <p id=\"msg\">Copied!</p>
+            <div class=\"flex\">
+                <button id=\"copyButton\" onclick=\"copy()\">
+                    <span class=\"material-symbols-outlined\">content_copy</span>
+                </button>
+                <p id='pwd'>$password</p>
+            </div>   
+        </div>";
     };
 }
 ?>
@@ -19,6 +28,7 @@ if (isset($_POST["size"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="scr/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css?v=2">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -26,27 +36,29 @@ if (isset($_POST["size"])) {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=content_copy" />
     <title>Password Generator</title>
 </head>
-<body>
-    <script>
-        function copy(){
-            var password = document.getElementById("pwd");
-            navigator.clipboard.writeText(password.value);
-        }
-    </script>
+<body class="flex">
+<script>
+    const copyButton = document.getElementById("copyButton");
+    copyButton.style.setProperty('--copied', 'none');
+
+    function copy() {
+        var password = document.getElementById("pwd");
+        navigator.clipboard.writeText(password.textContent);
+        copyButton.style.setProperty('--copied', 'block');
+    }
+
+    copyButton.addEventListener("click", copy);
+</script>
     <h1>Password Generator</h1>
-    <form method="post">
+    <form method="post" class="flex">
         <input type="number" name="size" min="4" max="30" placeholder="Password Length" required>
         <button type="submit">Gerar senha</button>
     </form>
-    <div>
-        <button>
-            <span onclick="copy()" class="material-symbols-outlined">content_copy</span>
-        </button>
         <?php 
             if (isset($_POST["size"])) {
                 echo generatePassword($size);
             }
         ?>
-    </div>
+
 </body>
 </html>
